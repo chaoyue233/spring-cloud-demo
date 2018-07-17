230623c4d2036d154934b0c865568ca362698562
wuchaoyue <wuchaoyue@rongyi.com>
2018-07-17 19:10:55 +0800

    添加 spring boot demo

---
+++ src/main/java/com/chaoyue/springCloudDemo/SpringCloudDemoApplication.java
@@ -0,0 +1,12 @@
+package com.chaoyue.springCloudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDemoApplication {
+    //test
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemoApplication.class, args);
+    }
+}
